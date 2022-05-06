20a88318d351e0001ae15de7274cd2729bd8e054
zhongyun <1422803768@qq.com>
2022-05-06 14:19:24 +0800

    'Test1'

---
+++ src/test/java/com/nowcoder/mavendemo01/AppTest.java
@@ -0,0 +1,20 @@
+package com.nowcoder.mavendemo01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
